884b1bc8c13b4e82fb5c256ba6582b56fb4c15a0
anna-rose <917566@isd624.org>
2018-07-23 20:58:45 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -21,15 +21,15 @@ puts #{phrase}.downcase
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = #{big_word}.length
 
 puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = #{big_word}+wow+mom
 
-new_letter_count = bigger_word.size
+new_letter_count = #{bigger_word}.lenth
 
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
@@ -37,13 +37,13 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 
 proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
 
-puts proper_sentence
+puts #{proper_sentence}.capitalize
 
 # Challenge 7: String Switcheroo
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+sentence = #{sentence}.gsub ('a', 'o')
 
 puts sentence
